82a3745b146b019f1259ac6ed48e77d8c9e5c24c
Israel Velez <201820883@tese.edu.mx>
2021-04-12 08:55:52 -0500

    Limpiar Mensajes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import{HEROES} from '../mock-heroes';
-import { HeroDetailComponent } from './../hero-detail/hero-detail.component';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +10,23 @@ import { HeroDetailComponent } from './../hero-detail/hero-detail.component';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes= HEROES;
   selectedHero?: Hero;
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {
-  }
-  onSelect(hero: Hero): void{
-    this.selectedHero=hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
